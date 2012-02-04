eaf01cc3d242c3dff5953e0106fc5360317a4bf3
Júlio César de Souza <julio@julioc.me>
2012-02-04 21:33:15 -0200

    initial commit

---
+++ README.md
@@ -0,0 +1,64 @@
+# Steam.js
+
+A Node.js library for the Steam Web API, with wrappers to deal with its methods, like those used on [Steam Mobile App](http://store.steampowered.com/mobile).
+
+## Install
+
+<pre>
+  git clone git://github.com/JulioC/steam.js.git 
+  cd steam.js
+  npm link
+</pre>
+
+## Example Usage
+
+```
+var steam = require('steam');
+
+steam.util.getServerInfo(function (e, d) {
+  console.log('Server time: ' + d.servertimestring);
+});
+
+steam.request({
+  interface: 'ISteamNews',
+  method: 'GetNewsForApp',
+  version: '0002',
+  secure: false,
+  get: {
+    appid: 24010,
+    maxlength: 128,
+    count: 3
+  }
+}, function (e, d) {
+  d.appnews.newsitems.forEach(function(news) {
+    console.log(news);
+  });
+});
+
+```
+
+## Function Reference
+
+### request(options, callback)
+
+Make a request for the Steam API with a method with the given parameters.
+
+If the first argument is a string, it will be threated as the `path` option. If it's an object, it should have at least the `interf` and `method` set. The available options are:
+
+* `interface`: the Steam API interface
+* `method`: the Steam API method 
+* `version`: version of the method to be called, format `XXXX`, defaults to `0001`
+* `path`: overwrites the interface, method and version options, with format `interface/method/vXXXX`
+* `secure`: if the request should be made via https, defaults to `true`
+* `get`: GET params for the HTTP request
+* `post`: POST params for the HTTP request
+
+Once the request is finished, the `callback` function will be called with 2 arguments, an error object (null if success) and the available data (parsed from json if success).
+
+### util.getServerInfo(callback)
+
+Shorthand for `request(SteamWebAPIUtil/GetServerInfo, callback)`. Get the current time on the server.
+
+### util.getSupportedAPIList(options, callback)
+
+Shorthand for `request(SteamWebAPIUtil/GetServerInfo, callback)`. Accepts an optional object with the `key` to be used on the request. Lists the available methods and their parameters.
---
+++ main.js
@@ -0,0 +1,123 @@
+var util = require('util'),
+  events = require('events'),
+  url = require('url'),
+  qs = require('querystring'),
+  request = require('request');
+
+function Steam() {
+  this.host = 'api.steampowered.com';
+}
+util.inherits(Steam, events.EventEmitter);
+
+var steam = new Steam();
+module.exports = steam;
+
+Steam.prototype.request = function (opt, callback) {
+  var self = this;
+
+  opt = opt || {};
+
+  if (typeof opt === 'string') {
+    var parts = opt.split('/');
+    opt = {
+      interface: parts[0],
+      method: parts[1],
+      version: parts[2]
+    };
+  }
+
+  var options = {
+      interface: opt.interface,
+      method: opt.method,
+      version: opt.version || '0001',
+      path: opt.path,
+      secure: typeof opt.secure !== 'undefined' ? opt.secure : true,
+      get: opt.get,
+      post: opt.post
+    },
+    req = {
+      url: url.format({
+        protocol: options.secure ? 'https' : 'http',
+        host: this.host,
+        pathname: options.path || [options.interface, options.method, 'v' + options.version].join('/'),
+        query: options.get
+      })
+    };
+
+  if (options.post) {
+    req.method = 'POST';
+    req.body = '?' + qs.stringify(options.post).toString('ascii');
+    req.headers = {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Host': options.url
+    };
+  }
+
+  request(req, function (e, r, b) {
+    var error = null,
+      data = null;
+
+    if (e) {
+      error = {
+        type: 'client',
+        info: e // we should return a proper messagehere
+      };
+    } else if (r.statusCode !== 200) {
+      error = {
+        type: 'request',
+        info: r.statusCode
+      };
+    } else {
+      try {
+        data = JSON.parse(b);
+      } catch (ex) {
+        error = {
+          type: 'parse',
+          info: 'failed to parse API response'
+        };
+        data = b;
+      }
+    }
+
+    if (data && data.error && data.error !== 'OK') {
+      error = {
+        type: 'steam',
+        info: data.error
+      };
+    }
+
+    if (callback) {
+      callback(error, data);
+    }
+
+    self.emit('response', error, data);
+  });
+
+  self.emit('request', options);
+};
+
+module.exports.util = {
+  getServerInfo: function (callback) {
+    steam.request('ISteamWebAPIUtil/GetServerInfo', callback);
+  },
+  getSupportedAPIList: function (opt, callback) {
+    var self = this;
+
+    opt = opt || {};
+
+    var params = {};
+    if (opt.key) {
+      params.key = opt.key;
+    } else {
+      // [API Glitch?]
+      // This will return some methods, without needing a key
+      params.client_id = 'none';
+    }
+
+    steam.request({
+      interface: 'ISteamWebAPIUtil',
+      method: 'GetSupportedAPIList',
+      get: params
+    }, callback);
+  }
+};
